3fe7a93af69784c256647c015e6102d602ba9e7d
Mihalkevitc <144014512+Mihalkevitc@users.noreply.github.com>
2023-12-09 05:38:47 +0300

    Update Curs_script_termin.js

--- Curs_script_termin.js
+++ Curs_script_termin.js
@@ -5,14 +5,17 @@ function toggleHeight(element, height, inset_text_n)
     var parent = element.parentNode;
     var inset_wrapper = document.getElementById("inset_wrapper");
     var inset_text = document.getElementById(inset_text_n);
+    var height_text = inset_text.offsetHeight;
+    alert(inset_text_n);
+    alert(height_text);
     if (parent.offsetHeight == offsetHeight) 
     {
-      parent.style.height = height+"px";
+      parent.style.height = (height_text+150)+"px";
       parent.style.backgroundColor = "white";
       parent.style.color = "black";
       parent.style.borderRadius = "15px";
       inset_text.style.visibility = "visible";
-      inset_wrapper.style.height = inset_wrapper.offsetHeight + height + "px";
+      inset_wrapper.style.height = inset_wrapper.offsetHeight + (height_text+150) + "px";
     } 
     else 
     {
@@ -21,6 +24,6 @@ function toggleHeight(element, height, inset_text_n)
       parent.style.backgroundColor = "rgb(13, 2, 15)";
       parent.style.color = "white";
       parent.style.borderRadius = "0";
-      inset_wrapper.style.height = (inset_wrapper.offsetHeight - height) + "px";
+      inset_wrapper.style.height = (inset_wrapper.offsetHeight - (height_text+150)) + "px";
     }
   }
